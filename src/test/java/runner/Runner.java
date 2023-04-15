beb9f4da7736ae3b85f0d60ecc9226f86d885cbb
hkartal <huseyin86kartal@gmail.com>
2023-04-15 19:14:40 +0300

    git ignore added

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 
 @CucumberOptions(
-        features = {"src/test/java/muratMovita/movitaAnaSayfa.feature"},
+        features = {"src/test/java/features"},
         glue = {"stepdefs/muratStepdefs"}
        // tags = "@WishList",
         //plugin = { "pretty",
