a268422044cc932ba03b4137db6a4b0390a78674
yasinmurat <muratyeni671@gmail.com>
2023-04-26 00:42:38 +0300

    Yasin Murat Movita TestCase5 Urunler Cozumler Button

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 
 @CucumberOptions(
-        features = {"src/test/java/muratMovita"},
+        features = {"src/test/java/features/muratMovita/movitaurunlerCozumler.feature"},
         glue = {"stepdefs"}
        // tags = "@test1"
         //plugin = { "pretty",
