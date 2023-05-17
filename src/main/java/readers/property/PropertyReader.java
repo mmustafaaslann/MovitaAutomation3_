052146727762b57aaafa5f617a2988321547c629
hkartal <huseyin86kartal@gmail.com>
2023-05-17 22:02:57 +0300

    PropertyReader reverted

--- src/main/java/readers/property/PropertyReader.java
+++ src/main/java/readers/property/PropertyReader.java
@@ -8,37 +8,19 @@ import java.util.Properties;
 public class PropertyReader {
 
     Properties prop = new Properties();
-<<<<<<< HEAD
-    FileReader fileReader;
-    FileWriter fileWriter;
     String file;
 
-    public static PropertyReader propertyReader(){
-        return propertyReader("config");
-    }
-
-    public static PropertyReader propertyReader(String fileName){
-        return new PropertyReader(fileName);
-    }
-
-    private PropertyReader(String fileName){
-        try {
-            this.file = "src/test/resources/datafiles/" + fileName + ".properties";
-=======
-    String file;
-
-    public static PropertyReader read(){
+    public static PropertyReader read() {
         return read("config");
     }
 
-    public static PropertyReader read(String fileName){
+    public static PropertyReader read(String fileName) {
         return new PropertyReader(fileName);
     }
 
-    public PropertyReader(String fileName){
+    public PropertyReader(String fileName) {
         try {
             file = "src/test/resources/datafiles/" + fileName + ".properties";
->>>>>>> 8245ab7e4f38d4e4d227ab46a65ba0b559de431b
             FileReader fileReader = new FileReader(file);
             prop.load(fileReader);
         } catch (IOException e) {
@@ -47,35 +29,26 @@ public class PropertyReader {
 
     }
 
-    public String get(String key){
+    public String get(String key) {
         return prop.getProperty(key);
     }
 
 
-    public void set(String key, String value){
+    public void set(String key, String value) {
         prop.setProperty(key, value);
     }
 
 
-    public void del(String key){
+    public void del(String key) {
         prop.remove(key);
     }
 
-    public void store(String comment){
+    public void store(String comment) {
         try {
-<<<<<<< HEAD
-            fileWriter = new FileWriter(file);
-=======
             FileWriter fileWriter = new FileWriter(file);
->>>>>>> 8245ab7e4f38d4e4d227ab46a65ba0b559de431b
             prop.store(fileWriter, comment);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 8245ab7e4f38d4e4d227ab46a65ba0b559de431b
